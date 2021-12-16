510d60a320f84cd81a82b5d2f61cb3462822982e
James Yang <26634873@qq.com>
2021-12-16 11:11:15 +0000

    add RUN_KILL_TIMEOUT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const cp = require('child_process');
 
-const { RUN_TIMEOUT = 3600 * 1000 } = process.env;
+const { RUN_TIMEOUT = 3600 * 1000, RUN_KILL_TIMEOUT = 100 } = process.env;
 const [command, checkString, ...ports] = process.argv.slice(2);
 
 main(command, checkString, ports);
@@ -13,19 +13,23 @@ function main (command, checkString, ports = []) {
         timeout: RUN_TIMEOUT,
     });
     let str = '';
+
+    const killPorts = () => {
+        for (const port of ports) {
+            const x = cp.spawnSync('bash', [
+                '-c',
+                `pid=$(lsof -i:${port} -stcp:listen | awk '$2~/^[0-9]+$/{print $2}'); [ "$pid" ] && kill $pid; true`,
+            ]);
+            const killOutput = `${x.stdout}${x.stderr}`.trim();
+            killOutput && console.log(`kill ${port} error:`, killOutput);
+        }
+    }
     const handler = type => data => {
         str += data;
         process[type].write(data);
         if (str.includes(checkString)) {
             str = '';
-            for (const port of ports) {
-                const x = cp.spawnSync('bash', [
-                    '-c',
-                    `pid=$(lsof -i:${port} -stcp:listen | awk '$2~/^[0-9]+$/{print $2}'); [ "$pid" ] && kill $pid; true`,
-                ]);
-                const killOutput = `${x.stdout}${x.stderr}`.trim();
-                killOutput && console.log(`kill ${port} error:`, killOutput);
-            }
+            setTimeout(killPorts, RUN_KILL_TIMEOUT)
         }
     };
     child.stdout.on('data', handler('stdout'));
